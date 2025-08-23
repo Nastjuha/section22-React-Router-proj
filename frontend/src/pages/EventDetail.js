b1acae51635c312b2641fea9efec2478b0118931
Anastasiia <lukianets.anastasiia@gmail.com>
2025-08-23 22:29:33 +0200

    Task 7: Output ID of selected event

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>{params.eventId}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
