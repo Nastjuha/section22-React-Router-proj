9c65852be1e070d85f66d91e115e95d95d6eba42
Anastasiia <lukianets.anastasiia@gmail.com>
2025-08-31 17:32:13 +0200

    Dynamic Routes & loader()s

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,37 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>Event Detail Page</h1>
-      <p>{params.eventId}</p>
-    </>
-  );
+  const data = useLoaderData();
+  //console.log("DATA: ", data);
+  //console.log("EVENT: ", data.event);
+
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+// react-router (calls loader() for us) actually passes an object to the loader function with 2 props: request and params
+// with the help of 'params' we can access all the route parameters values, as we can do the same in the component with useParams() hook
+export async function loader({ request, params }) {
+  const response = await fetch(
+    "http://localhost:8080/events/" + params.eventId
+  );
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: "Could not fetch details for selected event.",
+      }),
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+
+  // OR simply:     return fetch("http://localhost:8080/events/" + params.eventId);
+  // react-router will automatically wait for the Promise AND give us access to the data to which it resolves
+}
