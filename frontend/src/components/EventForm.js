e91556c3ce2b8911e8c2339daf21f570367a9d32
Anastasiia <lukianets.anastasiia@gmail.com>
2025-09-27 23:49:16 +0200

    Validating User Input & Outputting Validation Errors

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,8 +1,14 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+  Form,
+  useNavigate,
+  useNavigation,
+  useActionData,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
   // through useNavigation() hook we can get access to the data that was submitted through a form, also get what the current status of currently active transition is
@@ -19,6 +25,15 @@ function EventForm({ method, event }) {
     // Form will make sure that the browser default of sending a request to the server on form submission will be omitted, BUT
     // it will take that request and send it to the action function defined for the route that rendered this component
     <Form method="post" className={classes.form}>
+      {/* data won't be set if we haven't submitted form yet; (data is coming from an action) */}
+      {/* ch ecking for 'data' <=> if I submitted a form and an action returned some data, then check if i have nested obj - errors in my data */}
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
