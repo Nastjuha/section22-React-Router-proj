253014c995be4ab8edf8fe5f2240cd444f4d10a6
Anastasiia <lukianets.anastasiia@gmail.com>
2025-09-28 21:54:54 +0200

    Deferring Data Fetching with defer()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,26 @@
-import { useLoaderData, json } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  return <EventsList events={events} />;
+  return (
+    // Suspense c is used to provide a fallback whilst data is about to arrive
+    // here we show Suspense to wait while we are waiting this events to be fetched
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+        {/* we have to provide a f, that r-r will execute once the promise is resolved */}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -23,6 +28,13 @@ export async function loader() {
       status: 500,
     });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(), // returns a promise, we store it in "events" key
+  };
 }
